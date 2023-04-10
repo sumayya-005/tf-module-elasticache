b33f21204dabd0ff92f69edea81ea4c2186fdcb2
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-10 11:57:34 +0530

    updating ./tf-module-elasticache

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_elasticache_cluster" "example" {
+  for_each             = var.elasticache
+  cluster_id           = "cluster-example"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+}
+
+resource "aws_vpc" "foo" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tf-test"
+  }
+}
+
+resource "aws_subnet" "foo" {
+  vpc_id            = aws_vpc.foo.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-test"
+  }
+}
+
+resource "aws_elasticache_subnet_group" "bar" {
+  name       = "tf-test-cache-subnet"
+  subnet_ids = [aws_subnet.foo.id]
+}
