b627a480e3a954e3d5457267949aca52d2d3d415
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-17 18:50:50 +0530

    updating ./tf-module-elasticache

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_elasticache_cluster" "main" {
   engine_version       = var.engine_version
   port                 = 6379
   security_group_ids   = [aws_security_group.sg.id]
+  subnet_group_name    = aws_elasticache_subnet_group.default.name
 }
 
 resource "aws_elasticache_parameter_group" "default" {
